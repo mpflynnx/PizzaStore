34b0a0a971a33de9c16a691b96e907df5e6f7c54
mpflynnx <mpflynnx@gmail.com>
2023-06-29 10:09:43 +0100

    Feature : delete an item by id added code to DELETE an item by id

--- Program.cs
+++ Program.cs
@@ -42,5 +42,16 @@ app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
 	await db.SaveChangesAsync();
 	return Results.NoContent();
 });
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+	var pizza = await db.Pizzas.FindAsync(id);
+	if (pizza is null)
+	{
+		return Results.NotFound();
+	}
+	db.Pizzas.Remove(pizza);
+	await db.SaveChangesAsync();
+	return Results.Ok();
+});
 
 app.Run();
