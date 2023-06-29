59152d7284aebd160e70de638a03ae94009accd4
mpflynnx <mpflynnx@gmail.com>
2023-06-29 09:43:11 +0100

    Feature : Add EF Core to minimal API added in-memory database support for testing

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
-using PizzaStore.DB;
-
+using PizzaStore.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "PizzaStore API",
+		Description = "Making the Pizzas you love",
+		Version = "v1"
+	});
 });
 
 var app = builder.Build();
-
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -20,10 +25,6 @@ app.UseSwaggerUI(c =>
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
 app.Run();
